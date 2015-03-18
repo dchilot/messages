0642e9954dbd8d6c9c00114a81482a3aef017df2
dchilot <dchilot@hotmail.com>
2015-03-18 01:43:34 +0000

    Playing with a way to capture values from a message.

--- shooter/orwell/shooter/test/yaml2protobuf_test.py
+++ shooter/orwell/shooter/test/yaml2protobuf_test.py
@@ -7,6 +7,9 @@ import orwell.yaml2protobuf as y2p
 import pprint
 import yaml
 import orwell.messages.controller_pb2 as pb_controller
+import orwell.messages.robot_pb2 as pb_robot
+import orwell.messages.server_game_pb2 as pb_server_game
+import orwell.messages.server_web_pb2 as pb_server_web
 from pbjson.pbjson import pb2dict
 from pbjson.pbjson import dict2pb
 
@@ -116,3 +119,90 @@ message: !Input {{ "message": {{ "move": {{ "left": {left}, "right": {right} }},
         assert_equal(message.move.right, message2.protobuf_message.move.right)
         assert_equal(message.fire.weapon1, message2.protobuf_message.fire.weapon1)
         assert_equal(message.fire.weapon2, message2.protobuf_message.fire.weapon2)
+
+    def test_6(self):
+        import os
+        import sys
+        test_dir = os.path.dirname(os.path.realpath(__file__))
+        with open(os.path.join(test_dir, "conf.yaml"), 'r') as input:
+            data = yaml.load(input.read())
+            sys.stderr.write(str(data) + "\n")
+            for dico in data["messages"]:
+                #sys.stderr.write(" " + str(dico) + "\n")
+                for name, message in dico.items():
+                    sys.stderr.write("  " + str(name) + "\n")
+                    sys.stderr.write("  pb fields: " + str(message.protobuf_message.ListFields()) + "\n")
+                    sys.stderr.write("  pb:\n")
+                    sys.stderr.write("   " + "\n   ".join(str(message.protobuf_message).split("\n")) + "\n")
+                    if ("register" == name):
+                        message_register_in = message.protobuf_message
+                    sys.stderr.write("   extracted = " + str(message.key_map) + "\n")
+        message_register = pb_robot.Register()
+        message_register.temporary_robot_id = "TEMPID1"
+        message_register.video_url = "http://video.url:123"
+        message_register.image = "this is an image of the robot"
+        sys.stderr.write(str(message_register) + "\n")
+        sys.stderr.write(str(dir(message_register)) + "\n")
+        sys.stderr.write("fields: " + str(message_register.ListFields()) + "\n")
+        sys.stderr.write(str(message_register == message_register_in) + "\n")
+        sys.stderr.write(" " + str(message_register.temporary_robot_id) + "\n")
+        sys.stderr.write(" " + str(message_register.video_url) + "\n")
+        sys.stderr.write(" " + str(message_register.image) + "\n")
+        message_registered = pb_server_game.Registered()
+        message_registered.robot_id = "ROBOT1"
+        message_registered.team = pb_server_game.RED
+        sys.stderr.write(str(message_registered) + "\n")
+        sys.stderr.write(" " + str(message_registered.robot_id) + "\n")
+        sys.stderr.write(" " + str(message_registered.team) + "\n")
+
+    @staticmethod
+    def test_key_map():
+        pb_message = pb_controller.Input()
+        pb_message.move.left = 0.2
+
+
+        #pb_message.move.right = FakeInt("{test}")
+        pb_message.move.right = -0.5
+        pb_message.fire.weapon1 = False
+        pb_message.fire.weapon2 = True
+        yaml_content = """
+message: !CaptureInput
+    destination: TEST1
+    message:
+        move:
+            left: {left}
+            right: "{{right}}"
+        fire:
+            weapon1: {weapon1}
+            weapon2: {weapon2}
+""".format(
+            left=pb_message.move.left,
+            weapon1=pb_message.fire.weapon1,
+            weapon2=pb_message.fire.weapon2)
+        data = yaml.load(yaml_content)
+        message1 = data["message"]
+        import sys
+        sys.stderr.write("\n" + str(message1.key_map) + "\n")
+        assert_equal(message1.key_map["/move/left"], pb_message.move.left)
+        assert_equal(message1.key_map["/move/right"], '{right}')
+        assert_equal(message1.key_map["/fire/weapon1"], pb_message.fire.weapon1)
+        assert_equal(message1.key_map["/fire/weapon2"], pb_message.fire.weapon2)
+        pb_message.move.left = 1.0
+        message2 = y2p.Input(pb_message.SerializeToString())
+        sys.stderr.write(str(message2.key_map) + "\n")
+        assert_equal(message2.key_map["/move/left"], pb_message.move.left)
+        assert_equal(message2.key_map["/move/right"], pb_message.move.right)
+        assert_equal(message2.key_map["/fire/weapon1"], pb_message.fire.weapon1)
+        assert_equal(message2.key_map["/fire/weapon2"], pb_message.fire.weapon2)
+        diffs = message1.compute_differences(message2)
+        sys.stderr.write(str(diffs) + "\n")
+        assert_equal(
+            [('@destination', 'TEST1', None), ('/move/left', 0.2, 1.0)],
+            diffs)
+        sys.stderr.write(str(message1.captured) + "\n")
+
+def main():
+    MainTest.test_key_map()
+
+if ("__main__" == __name__):
+    main()
--- shooter/orwell/yaml2protobuf.py
+++ shooter/orwell/yaml2protobuf.py
@@ -1,3 +1,5 @@
+import re
+
 import yaml
 from pbjson.pbjson import pb2dict
 from pbjson.pbjson import dict2pb
@@ -6,6 +8,8 @@ import orwell.messages.controller_pb2
 import orwell.messages.robot_pb2
 import orwell.messages.server_game_pb2
 import orwell.messages.server_web_pb2
+
+import google.protobuf.descriptor as pb_descriptor
 
 from . import messages
 
@@ -38,10 +42,13 @@ class CustomMetaClass(type):
 
 
 class Base(object):
-    def __init__(self, payload):
+    CAPTURE_PATTERN = re.compile('{[^{].*[^}]}')
+
+    def __init__(self, payload, destination=None):
         self._message = self.PROTOBUF_CLASS()
         self._message.ParseFromString(payload)
         self.message = pb2dict(self._message)
+        self.destination = destination
 
     def load(self):
         self._message = dict2pb(self.PROTOBUF_CLASS, self.message)
@@ -68,6 +75,98 @@ class Base(object):
                 str(self.message),
             )
 
+    @property
+    def key_map(self):
+        if (not hasattr(self, '_key_map')):
+            path = []
+            path_stack = [""]
+            pb_stack = [self.protobuf_message]
+            self._key_map = {}
+            first  = True
+            while (pb_stack):
+                message = pb_stack.pop()
+                if (path_stack):
+                    path.append(path_stack.pop())
+                for field in message.ListFields():
+                    descriptor, value = field
+                    path.append(descriptor.name)
+                    if (descriptor.type in
+                            (pb_descriptor.FieldDescriptor.TYPE_GROUP,
+                            pb_descriptor.FieldDescriptor.TYPE_MESSAGE)):
+                        # nested message
+                        pb_stack.append(value)
+                        path_stack.append(path.pop())
+                    else:
+                        key = "/".join(path)
+                        self._key_map[key] = value
+                        path.pop()
+                if (not first):
+                    path.pop()
+                else:
+                    first = False
+        return self._key_map
+
+
+class Capture(object):
+    def __new__(cls, *args, **kwargs):
+        instance = object.__new__(cls, *args, **kwargs)
+        setattr(instance, 'captured', [])
+        # !CaptureMessage -> !Message
+        setattr(
+            instance,
+            'captured_yaml_tag',
+            instance.yaml_tag.replace('!Capture', '!'))
+        return instance
+
+    @property
+    def key_map(self):
+        if (not hasattr(self, '_key_map')):
+            path = []
+            path_stack = [""]
+            dico_stack = [self.message]
+            self._key_map = {}
+            first  = True
+            while (dico_stack):
+                dico = dico_stack.pop()
+                if (path_stack):
+                    path.append(path_stack.pop())
+                for dico_key, value in dico.items():
+                    path.append(dico_key)
+                    if (isinstance(value, dict)):
+                        # nested message
+                        dico_stack.append(value)
+                        path_stack.append(path.pop())
+                    else:
+                        key = "/".join(path)
+                        self._key_map[key] = value
+                        path.pop()
+                if (not first):
+                    path.pop()
+                else:
+                    first = False
+        return self._key_map
+
+
+    def compute_differences(self, other):
+        differences = []
+        captured = {}
+        if (self.captured_yaml_tag != other.yaml_tag):
+            differences.append(("@name", self.captured_yaml_tag, other.yaml_tag))
+        if (self.destination != other.destination):
+            differences.append(
+                ("@destination", self.destination, other.destination))
+        for key, reference_value in self.key_map.items():
+            other_value = other.key_map[key]
+            if (reference_value != other_value):
+                if ((isinstance(reference_value, str)) and
+                        (Base.CAPTURE_PATTERN.match(reference_value))):
+                    capture_name = reference_value[1:-1]
+                    captured[capture_name] = other_value
+                else:
+                    differences.append((key, reference_value, other_value))
+        self.captured.append(captured)
+        return differences
+
 
 # [[[cog
 #import cog
@@ -79,11 +178,15 @@ class Base(object):
 #import orwell.messages.server_game_pb2 as pb_server_game
 #import orwell.messages.server_web_pb2 as pb_server_web
 #
-#TEMPLATE = """
-#class {name}(yaml.YAMLObject, Base):
+#TEMPLATE = """class {name}(yaml.YAMLObject, Base):
 #   __metaclass__ = CustomMetaClass
 #   PROTOBUF_CLASS = {module}.{name}
 #   yaml_tag = u'!{name}'
+#
+#
+#class Capture{name}(yaml.YAMLObject, Capture):
+#   yaml_tag = u'!Capture{name}'
+#
 #"""
 #
 #def gen(class_description):
@@ -92,16 +195,22 @@ class Base(object):
 #   cog.outl(TEMPLATE.format(name=name, module=module))
 #
 #
+#cog.outl("\n")
 #map(gen, inspect.getmembers(pb_controller, inspect.isclass))
 #map(gen, inspect.getmembers(pb_robot, inspect.isclass))
 #map(gen, inspect.getmembers(pb_server_game, inspect.isclass))
 #map(gen, inspect.getmembers(pb_server_web, inspect.isclass))
 # ]]]
 
+
 class Fire(yaml.YAMLObject, Base):
    __metaclass__ = CustomMetaClass
    PROTOBUF_CLASS = orwell.messages.controller_pb2.Fire
    yaml_tag = u'!Fire'
+
+
+class CaptureFire(yaml.YAMLObject, Capture):
+   yaml_tag = u'!CaptureFire'
 
 
 class Hello(yaml.YAMLObject, Base):
@@ -110,10 +219,18 @@ class Hello(yaml.YAMLObject, Base):
    yaml_tag = u'!Hello'
 
 
+class CaptureHello(yaml.YAMLObject, Capture):
+   yaml_tag = u'!CaptureHello'
+
+
 class Input(yaml.YAMLObject, Base):
    __metaclass__ = CustomMetaClass
    PROTOBUF_CLASS = orwell.messages.controller_pb2.Input
    yaml_tag = u'!Input'
+
+
+class CaptureInput(yaml.YAMLObject, Capture):
+   yaml_tag = u'!CaptureInput'
 
 
 class Move(yaml.YAMLObject, Base):
@@ -122,10 +239,18 @@ class Move(yaml.YAMLObject, Base):
    yaml_tag = u'!Move'
 
 
+class CaptureMove(yaml.YAMLObject, Capture):
+   yaml_tag = u'!CaptureMove'
+
+
 class Colour(yaml.YAMLObject, Base):
    __metaclass__ = CustomMetaClass
    PROTOBUF_CLASS = orwell.messages.robot_pb2.Colour
    yaml_tag = u'!Colour'
+
+
+class CaptureColour(yaml.YAMLObject, Capture):
+   yaml_tag = u'!CaptureColour'
 
 
 class Register(yaml.YAMLObject, Base):
@@ -134,10 +259,18 @@ class Register(yaml.YAMLObject, Base):
    yaml_tag = u'!Register'
 
 
+class CaptureRegister(yaml.YAMLObject, Capture):
+   yaml_tag = u'!CaptureRegister'
+
+
 class Rfid(yaml.YAMLObject, Base):
    __metaclass__ = CustomMetaClass
    PROTOBUF_CLASS = orwell.messages.robot_pb2.Rfid
    yaml_tag = u'!Rfid'
+
+
+class CaptureRfid(yaml.YAMLObject, Capture):
+   yaml_tag = u'!CaptureRfid'
 
 
 class ServerRobotState(yaml.YAMLObject, Base):
@@ -146,10 +279,18 @@ class ServerRobotState(yaml.YAMLObject, Base):
    yaml_tag = u'!ServerRobotState'
 
 
+class CaptureServerRobotState(yaml.YAMLObject, Capture):
+   yaml_tag = u'!CaptureServerRobotState'
+
+
 class Access(yaml.YAMLObject, Base):
    __metaclass__ = CustomMetaClass
    PROTOBUF_CLASS = orwell.messages.server_game_pb2.Access
    yaml_tag = u'!Access'
+
+
+class CaptureAccess(yaml.YAMLObject, Capture):
+   yaml_tag = u'!CaptureAccess'
 
 
 class GameState(yaml.YAMLObject, Base):
@@ -158,10 +299,18 @@ class GameState(yaml.YAMLObject, Base):
    yaml_tag = u'!GameState'
 
 
+class CaptureGameState(yaml.YAMLObject, Capture):
+   yaml_tag = u'!CaptureGameState'
+
+
 class Goodbye(yaml.YAMLObject, Base):
    __metaclass__ = CustomMetaClass
    PROTOBUF_CLASS = orwell.messages.server_game_pb2.Goodbye
    yaml_tag = u'!Goodbye'
+
+
+class CaptureGoodbye(yaml.YAMLObject, Capture):
+   yaml_tag = u'!CaptureGoodbye'
 
 
 class Registered(yaml.YAMLObject, Base):
@@ -170,10 +319,18 @@ class Registered(yaml.YAMLObject, Base):
    yaml_tag = u'!Registered'
 
 
+class CaptureRegistered(yaml.YAMLObject, Capture):
+   yaml_tag = u'!CaptureRegistered'
+
+
 class Start(yaml.YAMLObject, Base):
    __metaclass__ = CustomMetaClass
    PROTOBUF_CLASS = orwell.messages.server_game_pb2.Start
    yaml_tag = u'!Start'
+
+
+class CaptureStart(yaml.YAMLObject, Capture):
+   yaml_tag = u'!CaptureStart'
 
 
 class Stop(yaml.YAMLObject, Base):
@@ -182,10 +339,18 @@ class Stop(yaml.YAMLObject, Base):
    yaml_tag = u'!Stop'
 
 
+class CaptureStop(yaml.YAMLObject, Capture):
+   yaml_tag = u'!CaptureStop'
+
+
 class Team(yaml.YAMLObject, Base):
    __metaclass__ = CustomMetaClass
    PROTOBUF_CLASS = orwell.messages.server_game_pb2.Team
    yaml_tag = u'!Team'
+
+
+class CaptureTeam(yaml.YAMLObject, Capture):
+   yaml_tag = u'!CaptureTeam'
 
 
 class Welcome(yaml.YAMLObject, Base):
@@ -194,15 +359,28 @@ class Welcome(yaml.YAMLObject, Base):
    yaml_tag = u'!Welcome'
 
 
+class CaptureWelcome(yaml.YAMLObject, Capture):
+   yaml_tag = u'!CaptureWelcome'
+
+
 class GetAccess(yaml.YAMLObject, Base):
    __metaclass__ = CustomMetaClass
    PROTOBUF_CLASS = orwell.messages.server_web_pb2.GetAccess
    yaml_tag = u'!GetAccess'
 
 
+class CaptureGetAccess(yaml.YAMLObject, Capture):
+   yaml_tag = u'!CaptureGetAccess'
+
+
 class GetGameState(yaml.YAMLObject, Base):
    __metaclass__ = CustomMetaClass
    PROTOBUF_CLASS = orwell.messages.server_web_pb2.GetGameState
    yaml_tag = u'!GetGameState'
+
+
+class CaptureGetGameState(yaml.YAMLObject, Capture):
+   yaml_tag = u'!CaptureGetGameState'
+
 
 # [[[end]]]
